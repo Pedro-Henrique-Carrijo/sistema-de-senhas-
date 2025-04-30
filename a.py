5eb7cc0fa5c8290d38510824b56c5bf50d8ec7f5
PedroHCarrijo <142514100+Pedro-Henrique-Carrijo@users.noreply.github.com>
2025-04-30 16:15:06 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,3 +1,6 @@
+'''Avaliação Projeto Python Franca Digital
+Integrantes: Pedro Henrique Carrijo e Felipe Duarte Santos'''
+
 import random
 import string
 
@@ -9,15 +12,14 @@ print("Tipos:\n"
       "4) Dígitos\n"
       "5) Todos os anteriores (recomendado)\n")
       
-tipo_de_senha: int = int(input("Digite o tipo de senha escolhido: "))
-tamanho_da_senha: int = int(input("Digite o tamanho desejado para a senha: "))
+senha: int = int(input("Digite o tipo de senha escolhido: "))
+tamanho: int = int(input("Digite o tamanho desejado para a senha: "))
 
 def gerar_senha(tamanho: int, usar_minusculas: bool = False, usar_maiusculas: bool = False,
                 usar_numeros: bool = False, usar_especiais: bool = False) -> str:
     if tamanho < 1:
         return "O tamanho da senha deve ser maior que 0."
     
-    # Conjuntos 
     caracteres = ""
     if usar_minusculas:
         caracteres += string.ascii_lowercase
@@ -31,7 +33,6 @@ def gerar_senha(tamanho: int, usar_minusculas: bool = False, usar_maiusculas: bo
     if not caracteres:
         return "Nenhum tipo de caractere selecionado."
 
-    # Garante que tem pelomenos um de cada tipo selecionado
     senha = []
     if usar_minusculas:
         senha.append(random.choice(string.ascii_lowercase))
@@ -42,30 +43,28 @@ def gerar_senha(tamanho: int, usar_minusculas: bool = False, usar_maiusculas: bo
     if usar_especiais:
         senha.append(random.choice("@#!&$#%*/ç.;:?¨()[]{}|,^~+-¨'_=°ªº"))
 
-    # Preenche o restante da senha
     senha += [random.choice(caracteres) for _ in range(tamanho - len(senha))]
     
-    # Embaralha a senha para evitar previsibilidade
     random.shuffle(senha)
 
     return "".join(senha)
 
-# Lógica pra /determinar os tipos de caracteres com base na escolha do usuário
-if tipo_de_senha == 1:
+
+if senha == 1:
     print("============== SENHA ==============\n"
-          "Copie sua senha: " + gerar_senha(tamanho_da_senha, usar_minusculas=True))
-elif tipo_de_senha == 2:
+          "Copie sua senha: " + gerar_senha(tamanho, usar_minusculas=True))
+elif senha == 2:
     print("============== SENHA ==============\n"
-          "Copie sua senha: " + gerar_senha(tamanho_da_senha, usar_maiusculas=True))
-elif tipo_de_senha == 3:
+          "Copie sua senha: " + gerar_senha(tamanho, usar_maiusculas=True))
+elif senha == 3:
     print("============== SENHA ==============\n"
-          "Copie sua senha: " + gerar_senha(tamanho_da_senha, usar_especiais=True))
-elif tipo_de_senha == 4:
+          "Copie sua senha: " + gerar_senha(tamanho, usar_especiais=True))
+elif senha == 4:
     print("============== SENHA ==============\n"
-          "Copie sua senha: " + gerar_senha(tamanho_da_senha, usar_numeros=True))
-elif tipo_de_senha == 5:
+          "Copie sua senha: " + gerar_senha(tamanho, usar_numeros=True))
+elif senha == 5:
     print("============== SENHA ==============\n"
-          "Copie sua senha: " + gerar_senha(tamanho_da_senha, usar_minusculas=True,
+          "Copie sua senha: " + gerar_senha(tamanho, usar_minusculas=True,
                                            usar_maiusculas=True, usar_numeros=True, usar_especiais=True))
 else:
     print("Tipo de senha inválido, tente novamente...")
